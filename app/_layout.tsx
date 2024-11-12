e9a94125a1ab590d9b9c1e737ca070d112bc919a
Cameron Silver <cameron@txidigital.com>
2024-11-12 14:38:11 -0600

    Add light/dark colours to the stack navigation header.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import ColorScheme from "./theme";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colors = ColorScheme(useColorScheme())
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+        },
+        headerTintColor: colors.text,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}/>
+  )
 }
