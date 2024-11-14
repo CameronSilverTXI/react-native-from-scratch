c14de37ffb9cf7531531704b71d1ae6acc36e426
Cameron Silver <cameron@txidigital.com>
2024-11-14 12:14:34 -0600

    Add ReactQuery

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,26 @@
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import ColorScheme from "@/theme";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function RootLayout() {
   const colors = ColorScheme(useColorScheme())
+
+  // Setup the ReactQuery client.
+  const queryClient = new QueryClient()
+
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.background,
-        },
-        headerTintColor: colors.text,
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}/>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.background,
+          },
+          headerTintColor: colors.text,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}/>
+    </QueryClientProvider>
   )
 }
